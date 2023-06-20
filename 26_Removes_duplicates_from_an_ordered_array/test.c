b6cd58b3f4d831886cf3abbc782284c6a327105e
Amberyuyuyu <135623522+Amberyuyuyu@users.noreply.github.com>
2023-06-20 11:41:11 +0800

    Update test.c

--- 26_Removes_duplicates_from_an_ordered_array/test.c
+++ 26_Removes_duplicates_from_an_ordered_array/test.c
@@ -1,7 +1,11 @@
 
 int removeDuplicates(int* nums, int numsSize) {
+    if(numsSize == 0){
+        return 0;
+    }
     int slow = 0;
     int fast = 0;
+    
     while (fast < numsSize) {
         if (nums[fast] != nums[slow]) {
             nums[++slow] = nums[fast];
