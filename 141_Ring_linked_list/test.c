156da8a06d9851b9b4c5d763466af11dd8ce38f4
Amberyuyuyu <18635968746@163.com>
2023-06-23 16:32:07 +0800

    V8

---
+++ 141_Ring_linked_list/test.c
@@ -0,0 +1,20 @@
+
+
+ // Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     struct ListNode *next;
+ };
+
+bool hasCycle(struct ListNode* head) {
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false;
+}
