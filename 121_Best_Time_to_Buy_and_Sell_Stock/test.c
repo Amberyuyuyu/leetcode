47f8cea62a94519258a68f2edac6872c1adeed72
Amberyuyuyu <18635968746@163.com>
2023-07-11 12:38:57 +0800

    V21

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock/test.c
@@ -0,0 +1,13 @@
+int maxProfit(int* prices, int pricesSize) {
+    int min = prices[0];
+    int max = 0;
+    for (int i = 0; i < pricesSize; i++) {
+        if (prices[i] < min) {
+            min = prices[i];
+        }
+        else if (prices[i] - min > max) {
+            max = prices[i] - min;
+        }
+    }
+    return max;
+}
