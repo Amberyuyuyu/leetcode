80e227730085dc6d5338aa040bb0e879edb4650a
Amberyuyuyu <18635968746@163.com>
2023-08-21 18:52:25 +0800

    V61

---
+++ 2236_ Root_Equals_Sum_of_Children/test.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool checkTree(struct TreeNode* root) {
+    if (root->left->val + root->right->val == root->val) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
