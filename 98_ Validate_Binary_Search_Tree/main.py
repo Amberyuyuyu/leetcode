d4938e14d55ee55d047a6753eeafe8e25d1d1174
Amberyuyuyu <18635968746@163.com>
2023-08-27 23:00:07 +0800

    V78

---
+++ 98_ Validate_Binary_Search_Tree/main.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    pre = float('-inf')
+
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        if not self.isValidBST(root.left):
+            return False
+        if root.val <= self.pre:
+            return False
+        self.pre = root.val
+        return self.isValidBST(root.right)
