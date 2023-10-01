f362e84c61d0d0c901eec81dd35fb34549853b2d
Amberyuyuyu <18635968746@163.com>
2023-10-01 21:40:28 +0800

    V139

---
+++ 322_Coin_Change/test.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i < coins.size(); i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                if (dp[j - coins[i]] != INT_MAX) {
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        if (dp[amount] != INT_MAX) {
+            return dp[amount];
+        }
+        return -1;
+    }
+
+};
