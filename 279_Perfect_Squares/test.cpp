e8e31cc5c280c325e7a0f45f4341dd41e03cae3c
Amberyuyuyu <18635968746@163.com>
2023-10-02 19:56:24 +0800

    V142

---
+++ 279_Perfect_Squares/test.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i * i <= n; i++) {
+            for (int j = i * i; j <= n; j++) {
+                dp[j] = min(dp[j], dp[j - i * i] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
