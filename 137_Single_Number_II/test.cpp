6b1c93bd6af3db2bf9344eb34e239919e32f8b1c
Amberyuyuyu <18635968746@163.com>
2023-10-09 11:40:06 +0800

    V175

---
+++ 137_Single_Number_II/test.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int num : nums) {
+                sum += ((num >> i) & 1);
+            }
+            if (sum % 3 == 1) {
+                res |= (1 << i);
+            }
+        }
+        return res;
+    }
+};
