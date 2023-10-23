b92e455cb38acc2c73683fdf11a57b7da7557c81
Amberyuyuyu <18635968746@163.com>
2023-10-23 19:13:51 +0800

    V218

---
+++ 867_Transpose_Matrix/test.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> res(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                res[i][j] = matrix[j][i];
+            }
+        }
+        return res;
+    }
+};
