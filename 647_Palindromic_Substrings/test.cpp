47b75af22fc251530b68d810157f3e41b1b50b1f
Amberyuyuyu <18635968746@163.com>
2023-11-08 17:26:12 +0800

    V240

---
+++ 647_Palindromic_Substrings/test.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        int result = 0;
+        for (int i = s.size() - 1; i >= 0; --i) {
+            for (int j = i; j < s.size(); ++j) {
+                if (s[i] == s[j]) {
+                    if (j - i <= 1) {
+                        result++;
+                        dp[i][j] = true;
+                    }
+                    else if (dp[i + 1][j - 1]) {
+                        result++;
+                        dp[i][j] = true;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
